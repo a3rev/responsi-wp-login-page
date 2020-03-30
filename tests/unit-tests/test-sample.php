0d409a9c75c857c098f41e0fec55aff36a6096b0
a3rev <mrkunau@gmail.com>
2020-03-30 18:48:23 +0700

    Unit Test

--- tests/unit-tests/test-sample.php
+++ tests/unit-tests/test-sample.php
@@ -14,23 +14,10 @@ class a3Rev_Tests_Sample extends WP_UnitTestCase {
 	 * A single example test.
 	 */
 
-	function test_responsi_theme() {
-		$output = 1;
-
-		$this->assertTrue( defined( 'RESPONSI_FRAMEWORK_VERSION' ) );
-	}
-
 	function test_sample() {
 		$output = 1;
 
 		$this->assertEquals( 1 , $output );
-	}
-
-	function test_responsi_css() {
-		global $responsi_wp_login_page;
-		$output = (string)$responsi_wp_login_page->responsi_build_dynamic_css();
-		$this->assertStringContainsString( 'html body.login form, html body.login form.loginform, html body.login #login form.loginform' , $output );
-
 	}
 
 }
