703eaa0af104883d75e1866d8398b70ffae82faf
a3rev <mrkunau@gmail.com>
2020-03-31 17:16:45 +0700

    Unit Test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,10 +11,10 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+/*if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
 	exit( 1 );
-}
+}*/
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
